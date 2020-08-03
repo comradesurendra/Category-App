ee230c3b9a7a7271fcf41a58ca3f5c005b94f536
Surendra Kumar <32401892+comradesurendra@users.noreply.github.com>
2020-08-03 15:26:58 +0530

    ui swap added

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,15 +1,27 @@
 import React from "react"
-import ToData from '../data/todo.json'
-import './todo.css'
+import ToData from "../data/todo.json"
+import "./todo.css"
+import { SwipeableList, SwipeableListItem } from "@sandstreamdev/react-swipeable-list"
+import "@sandstreamdev/react-swipeable-list/dist/styles.css"
 function Todo() {
   return (
-    <div className='todo-container'>
+    <div className="todo-container">
       {ToData.map((postData, index) => {
-        return <div className='todo-card'>
-          <h4>{postData.topic}</h4>
-          <p>{postData.dis}</p>
-          <h5>{postData.time}</h5>
-        </div>
+        return (
+          <SwipeableList style={{ "background-color": "#fff" }}>
+            <SwipeableListItem
+              swipeLeft={{
+                content: <div className="hidden-stat">{postData.status}</div>,
+              }}
+            >
+              <div className="todo-card">
+                <h4>{postData.topic}</h4>
+                <p>{postData.dis}</p>
+                <h5>{postData.time}</h5>
+              </div>
+            </SwipeableListItem>
+          </SwipeableList>
+        )
       })}
     </div>
   )
